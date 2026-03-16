06b78231edea33fba9c9b7f804c59456df1e2f5c
Shambhu_Prasad_Chaudhary-at-954145087675 <shambhuchu21@gmail.com>
2026-03-16 08:57:28 +0545

    changes in reset password

--- src/pages/ResetPassword.tsx
+++ src/pages/ResetPassword.tsx
@@ -3,12 +3,13 @@ import { Card, CardContent } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { ErrorMessage, Field, Form, Formik } from "formik";
 import { Building2, Eye, EyeOff } from "lucide-react";
-import { useState } from "react";
+import { useLayoutEffect, useState } from "react";
 import * as Yup from "yup";
 import { toast } from "sonner";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import BASE_URL from "@/config/config";
 import axios from "axios";
+import { IoIosWarning } from "react-icons/io";
 const schema = Yup.object({
   newPassword: Yup.string().min(6).required("New Password required"),
 });
@@ -16,8 +17,18 @@ export default function ResetPassword() {
   const [showNewPassword, setShowNewPassword] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
   const [searchParams] = useSearchParams();
+  const [data, setData] = useState<any>({});
   const token = searchParams.get("token");
   const navigate = useNavigate();
+  const validateRestPassword = async () => {
+    const res = await axios.get(
+      `${BASE_URL}/api/v3/unified/reset-password/validate?token=${token}`,
+    );
+    setData(res);
+  };
+  useLayoutEffect(() => {
+    validateRestPassword();
+  }, []);
   const handleResetPassword = async (values: any, { setSubmitting }: any) => {
     setErrorMessage(""); // Clear previous error
     const dtoFormData = {
@@ -38,7 +49,8 @@ export default function ResetPassword() {
       setSubmitting(false);
     }
   };
-  return (
+  console.log("data", data);
+  return data?.data?.valid ? (
     <div className="min-h-screen bg-gradient-to-br from-primary/5 via-background to-accent/5 flex flex-col">
       <div className="flex-1 flex items-center justify-center p-4">
         <div className="w-full max-w-md">
@@ -48,6 +60,7 @@ export default function ResetPassword() {
             </div>
             <h1 className="text-2xl font-bold">Reset password</h1>
           </div>
+
           <Card
             className="shadow-xl
           pt-7"
@@ -104,6 +117,21 @@ export default function ResetPassword() {
               )}
             </Formik>
           </Card>
+        </div>
+      </div>
+    </div>
+  ) : (
+    <div className="min-h-screen bg-gradient-to-br from-primary/5 via-background to-accent/5 flex flex-col">
+      <div className="flex-1 flex items-center justify-center p-4">
+        <div className="w-full max-w-md">
+          <div className="text-center mb-8">
+            <div className="inline-flex w-16 h-16 rounded-2xl bg-primary text-primary-foreground items-center justify-center mb-4">
+              <IoIosWarning className="w-10 h-10" />
+            </div>
+            <h1 className="text-2xl font-bold text-red-500">
+              Invalid, expired, or already used token
+            </h1>
+          </div>
         </div>
       </div>
     </div>
